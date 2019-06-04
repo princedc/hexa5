0721207f2aa1801799cd37ec71fd95e7641be2d8
unknown <prince.devasitham@gmail.com>
2019-06-03 22:08:01 -0500

    update package.json

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/Hexamatics'));
 
-app.listen(process.env.PORT | 8080);
+app.listen(process.env.PORT || 8080);
 
 //PathLocationStategy
 
 app.get('/*',function(req,res){
-    res.sendfile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Hexamatics/index.html'));
 
 })
 
